82a10fd9fd96f757e97318df70c9e26587fe2ac5
Maximiliano Greco <mgreco@ets.es>
2019-09-04 10:10:57 +0200

    Split install_requires in many lines

--- setup.py
+++ setup.py
@@ -1,6 +1,8 @@
 from setuptools import setup, find_packages
 
 setup(
-        name="pkgB",
-        install_requires=["numpy==1.13"],
-        )
+    name="pkgB",
+    install_requires=[
+        "numpy==1.13"
+        ],
+    )
